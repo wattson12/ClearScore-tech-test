9af8d0bc879a0b4e72294951332ab70259032ebe
Sam Watts <samuel.watts@gmail.com>
2018-03-20 09:18:10 +0000

    added credit score view'

---
+++ ClearScoreTask/Screens/Credit Score/CreditScoreViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreditScoreViewController.swift
+//  ClearScoreTask
+//
+//  Created by Sam Watts on 20/03/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import UIKit
+
+final class CreditScoreView: UIView {
+
+}
+
+class CreditScoreViewController: BaseViewController {
+
+    override func loadView() {
+        self.view = CreditScoreView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
